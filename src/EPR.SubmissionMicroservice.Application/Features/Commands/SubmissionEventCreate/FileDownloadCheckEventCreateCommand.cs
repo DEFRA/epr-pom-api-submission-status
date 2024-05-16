47e6a90e35ad4344a52ca17adf97666565deab61
Nadir Khan <nadir.2.khan.external@eviden.com>
2024-05-16 22:44:24 +0100

    261951: regulator review download implementation

---
+++ src/EPR.SubmissionMicroservice.Application/Features/Commands/SubmissionEventCreate/FileDownloadCheckEventCreateCommand.cs
@@ -0,0 +1,18 @@
+﻿using EPR.SubmissionMicroservice.Data.Enums;
+
+namespace EPR.SubmissionMicroservice.Application.Features.Commands.SubmissionEventCreate;
+
+public class FileDownloadCheckEventCreateCommand : AbstractSubmissionEventCreateCommand
+{
+    public override EventType Type => EventType.FileDownloadCheck;
+
+    public string UserEmail { get; set; }
+
+    public string ContentScan { get; set; }
+
+    public Guid FileId { get; set; }
+
+    public string FileName { get; set; }
+
+    public SubmissionType SubmissionType { get; set; }
+}
