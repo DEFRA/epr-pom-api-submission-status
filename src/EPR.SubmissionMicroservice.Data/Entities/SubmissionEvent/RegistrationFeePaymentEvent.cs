363a2c02000099efd63affcbd30473e00ab7837b
James Atherton <james.atherton@equalexperts.com>
2025-12-09 14:45:59 +0000

    Adding filter exception for Large Cso registrations that occured before the addition of RegistrationJourney to the Submission

--- src/EPR.SubmissionMicroservice.Data/Entities/SubmissionEvent/RegistrationFeePaymentEvent.cs
+++ src/EPR.SubmissionMicroservice.Data/Entities/SubmissionEvent/RegistrationFeePaymentEvent.cs
@@ -10,7 +10,7 @@ public class RegistrationFeePaymentEvent : AbstractSubmissionEvent
 
     public string? ApplicationReferenceNumber { get; set; }
 
-    public string PaymentMethod { get; set; }
+    public string PaymentMethod { get; set; } = "";
 
     public string PaymentStatus { get; set; }
 
